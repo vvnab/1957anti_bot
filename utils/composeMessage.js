a606ae088057f70ac8d458816805cabe2a1ea83c
Виталий Набережный <naberezhniy@taxi21.ru>
2018-09-17 18:42:47 +0300

    --

--- utils/composeMessage.js
+++ utils/composeMessage.js
@@ -5,7 +5,7 @@ const composeMessage = post => {
   let message;
   post.title = _.escape(post.title);
   const tme = `https://t.me/iv?url=${_.escape(post.link)}&rhash=db44e730036517`;
-  message = `<b>${post.author}</b>\n<a href="${post.link}">${post.title}</a>\n<a href="${tme}">--------</a>`;
+  message = `<b>${post.author}</b>\n<a href="${tme}">***</a>\n<a href="${post.link}">${post.title}</a>`;
   return message;
 }
 
