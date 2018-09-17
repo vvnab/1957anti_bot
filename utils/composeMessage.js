104cdfacad292528539f07f589b23774f31b229e
Виталий Набережный <naberezhniy@taxi21.ru>
2018-09-17 18:33:10 +0300

    --

--- utils/composeMessage.js
+++ utils/composeMessage.js
@@ -4,7 +4,8 @@ const _ = require('lodash');
 const composeMessage = post => {
   let message;
   post.title = _.escape(post.title);
-  message = `<a href="https://t.me/iv?url=${_.escape(post.link)}&rhash=db44e730036517">${post.title}</a>`;
+  const tme = `https://t.me/iv?url=${_.escape(post.link)}&rhash=db44e730036517`;
+  message = `<b>${post.author}</b>\n<a href="post.link">${post.title}</a>`;
   return message;
 }
 
